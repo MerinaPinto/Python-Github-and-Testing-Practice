6ec08bcb360c5b8535cc8f07edddc0f101b4c4c2
Merina Pinto <pintofamily2012@gmail.com>
2026-02-09 23:23:34 -0500

    Update test_mangman2.py

--- test_mangman2.py
+++ test_mangman2.py
@@ -1,1 +1,25 @@
+import mangman2
+
+
+def test_create_game_lives():
+    lives, blank_space, guessed_letters = mangman2.create_game("dog")
+    assert lives == 6
+
+
+def test_create_game_blank_spaces():
+    lives, blank_space, guessed_letters = mangman2.create_game("dog")
+    assert blank_space == ["_", "_", "_"]
+
+
+def test_create_game_guessed_letters_empty():
+    lives, blank_space, guessed_letters = mangman2.create_game("dog")
+    assert guessed_letters == []
+
+
+def test_game_over_win():
+    assert mangman2.game_over(["d", "o", "g"], 3) == True
+
+
+def test_game_over_loss():
+    assert mangman2.game_over(["_", "_", "_"], 0) == True
 
